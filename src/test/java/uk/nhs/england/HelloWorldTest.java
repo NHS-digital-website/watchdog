fde1856ec07cf5594503bee4e0f84d4421887384
GeoffHayward <geoffrey.hayward@nhs.net>
2023-09-06 11:33:35 +0100

    [DW-3431] Adds a basic maven project

---
+++ src/test/java/uk/nhs/england/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package uk.nhs.england;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(false);
+    }
+
+}
