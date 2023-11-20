ed8d0e9ba3a4e7beaee49037c73dca8d37f9a35c
lekeDevOps <lesley.kelly2@nhs.net>
2023-11-20 16:18:18 +0000

    [DW-3473] Optimise the imports

--- src/test/java/uk/nhs/england/dnslookups/ResolveDNSLookUpTextRecordTest.java
+++ src/test/java/uk/nhs/england/dnslookups/ResolveDNSLookUpTextRecordTest.java
@@ -7,6 +7,7 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.*;
 
 public class ResolveDNSLookUpTextRecordTest {
